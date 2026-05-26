514ba5620d5842ebf0e33725acbdbf208da266e7
Weibo Lin <230587150+Weibo-student@users.noreply.github.com>
2026-05-26 20:11:17 +0000

    seperate the main menu from the main

---
+++ new_duckington/main.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export GAME_ROOT="$(dirname "$0")"
+source "$GAME_ROOT/screens/main_menu.sh"
+
+while [[ $use_input != "valid" ]]
+do
+	clear
+display_title
+display_inital_menu
+get_user_option
+get_and_check_input_is_valid
+done
