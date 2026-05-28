97cc709aab94aab0c55750f689f710d68a1725a6
Jashan Singh <180464234+JashSingh09@users.noreply.github.com>
2026-05-28 10:52:01 -0500

    did stuff

--- new_duckington/rooms/shop_room.sh
+++ new_duckington/rooms/shop_room.sh
@@ -3,6 +3,9 @@
 export GAME_ROOT="$(dirname "$0")/.."
 source "$GAME_ROOT/data/player_data.sh"
 
+source "$GAME_ROOT/data/items/items_manager.sh"
+
+
 # Regular colours
 BLACK='\033[0;30m'
 RED='\033[0;31m'
@@ -21,46 +24,55 @@ BROWN='\033[0;33m'   # terminals fake brown using dark yellow/orange
 NC='\033[0m'
 
 shop() {
-
 	echo -e "${RED}===================${NC}"
 	echo -e "${CYAN}The Shop...${NC}"
 	echo -e "${RED}===================${NC}"
 	echo 
 	echo
 	while true; do
-
-	printf "What do you want to do"
-	echo
-	echo "-------------------"
-	echo -e "${YELLOW}[1]  Buy $item1${NC}" 
-	echo -e "${GREEN}[2]  Buy $item2${NC}"
-	echo -e "${BLUE}[4]  Buy $item3${NC}"
-	echo -e "${PINK}[4]  Buy $item4${NC}"
-	echo -e "${RED}[5]  EXIT SHOP${NC}"
-	read shop_action
-	case $shop_action in
-		1)
-			PLAYER_ITEMS+=($item1)
-			echo ${PLAYER_ITEMS[@]}
-			;;
-		2)
-			PLAYER_ITEMS+=($item2)
-			echo ${PLAYER_ITEMS[@]}
-			;;
-		3)
-			;;
-		4)
-			;;
-		5)	
-			break
-
-			;;
-		*) 
+		printf "What do you want to do"
+		echo
+		echo "-------------------"
+		echo -e "${YELLOW}[1]  Buy $item1${NC}" 
+		echo -e "${GREEN}[2]  Buy $item2${NC}"
+		echo -e "${BLUE}[4]  Buy $item3${NC}"
+		echo -e "${PINK}[4]  Buy $item4${NC}"
+		echo -e "${RED}[5]  EXIT SHOP${NC}"
+		read shop_action
+		case $shop_action in
 			
-			;;
+			1)
+				PLAYER_ITEMS+=($item1)
+				echo ${PLAYER_ITEMS[@]}
+				;;
+			
+			2)
+				PLAYER_ITEMS+=($item2)
+				echo ${PLAYER_ITEMS[@]}
+				;;
+			
+			
+			3)	
+				PLAYER_ITEMS+=($item3)
+				echo ${PLAYER_ITEMS[@]}
+				;;
+			
+			4)
+				PLAYER_ITEMS+=($item4)
+				echo ${PLAYER_ITEMS[@]}
+				;;
+			
+			5)	
+				break
+
+				;;
+			*) 
+
+				;;
 
 
-	esac
+		esac
+	done
 
 
 
