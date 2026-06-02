9e89aaa83381afa4ad64807985937bb583764a74
Jashan Singh <180464234+JashSingh09@users.noreply.github.com>
2026-06-02 10:25:23 -0500

    fixed what weibo did to me

--- new_duckington/rooms/room_manager.sh
+++ new_duckington/rooms/room_manager.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export GAME_ROOT="$(dirname "$0")/.."
+source "$GAME_ROOT/data/enemy_data.sh"
 combat_rooms=("small" "normal" "huge" "shop" "event")
 declare -a room_list
 
@@ -24,28 +25,68 @@ initialize_rooms() {
 	shuf -e "${room_list[@]}" 1> /dev/null   #shuf sends the output nowhere because it echos for some reason
 }
 
-initialize_rooms
+#initialize_rooms UNCOMMENT THIS LATER
 echo ${room_list[@]}
 
 
 
 #=================================================================
 
-#need to make a list of available enemies, 
+# #need to make a list of available enemies, 
 
-declare -a ENEMY_LIST
-#ls $GAME_ROOT/data/enemies/
-for file in $GAME_ROOT/data/enemies/*; do
-	if [[ "$(basename "$file")" == 'ememy_example.sh' ]]; then
-		continue
-    elif [[ -f "$file" ]]; then
-        ENEMY_LIST+=("$(basename "$file")")
-    fi
-done
-echo ${ENEMY_LIST[@]}
-
-
+# declare -a ENEMY_LIST
+# #ls $GAME_ROOT/data/enemies/
+# for file in $GAME_ROOT/data/enemies/*; do
+# 	if [[ "$(basename "$file")" == 'ememy_example.sh' ]]; then
+# 		continue
+#     elif [[ -f "$file" ]]; then
+#         ENEMY_LIST+=("$(basename "$file")")
+#     fi
+# done
+# echo ${ENEMY_LIST[@]}
 
 
+#=========================================
+declare -a ENEMY_LIST=(
+    "BEAR_GRUNT"
+    "EARTH_BEAR"
+    "IRON_BEAR"
+    "GRIM_BEAR"
+    "HOLLOW_BEAR"
+    "GRIZZLED_MAULER"
+    "WATER_BEAR"
+    "FIRE_BEAR"
+    "AIR_BEAR"
+)
+
+random_enemy=${ENEMY_LIST[$((RANDOM % ${#ENEMY_LIST[@]}))]}
+
+echo "$random_enemy"
+
+declare -n enemy="$random_enemy"
+
+echo "${enemy[name]}"
+echo "${enemy[hp]}"
+echo "${enemy[atk]}"
 
 
+
+
+
+
+
+#i need to setup boss_room
+
+boss_fight(){
+
+
+}
+
+
+
+
+
+final_bossfight() {
+
+
+}
